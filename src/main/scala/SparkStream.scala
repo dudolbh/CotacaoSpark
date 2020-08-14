b7da23bef85429ba8ec680785e2bdbc8eb5521e4
Eduardo Ney <dudolbh@gmail.com>
2020-08-14 19:15:59 -0300

    Schema json

--- src/main/scala/SparkStream.scala
+++ src/main/scala/SparkStream.scala
@@ -6,6 +6,7 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.execution.streaming.FileStreamSource.Timestamp
 import org.apache.spark.sql.functions.from_json
 import org.apache.spark.sql.kafka010._
+import org.apache.spark.sql.types.{StructField, StructType}
 import org.mortbay.util.ajax.JSON
 
 object SparkStream extends App{
@@ -21,6 +22,9 @@ object SparkStream extends App{
 
   import spark.implicits._
 
+  val schema = spark.read.json("C:\\Users\\eduar\\OneDrive\\PosEngDados\\Proj Aplicado\\Arquivos\\Sprint 4\\cotacao.json").schema
+  println(schema)
+
   val dfKafka = spark.readStream
     .format("kafka")
     .option("kafka.bootstrap.servers", bootstrapServers)
@@ -30,10 +34,12 @@ object SparkStream extends App{
     .option("maxOffsetsPerTrigger", 10)
     .load()
 
-  val dflinha = dfKafka.selectExpr("CAST(key AS STRING)", "to_json(CAST(value AS STRING))", "timestamp")
+  val dflinha = dfKafka.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "timestamp")
     .as[(String, String, Timestamp)]
 
-  val dflinhaJson = dfKafka.selectExpr("CAST(key AS STRING)", "from_json(CAST(value AS STRING))", "timestamp")
+  val dflinhaJson = dflinha.select(col("key"), col("timestamp"),from_json(col("value"),schema).as("value"))
+
+  //val dflinhaJson = dfKafka.select($"key",from_json($"value"), $"timestamp")
 
   //val dflinhaJson = dfKafka.select(from_json(col("data")).select(from_json(col("data"),schema).as("data"))
 
